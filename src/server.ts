14733c90710455b1cc0cdd01de42ba9f5390be73
Ben Jackson <bjackson2@gmail.com>
2020-07-11 14:23:48 -0400

    Adds CORS for local dev, wires query to database

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import {ApolloServer, gql} from 'apollo-server-express';
 import compression from 'compression';
+import cors from 'cors';
 import {query} from './db';
 
 dotenv.config();
@@ -15,18 +16,24 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World',
+    hello: async () => {
+      const res = await query('SELECT NOW()');
+      return res.rows[0].now;
+    },
   },
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
 
 const app = express();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
 app.use(compression());
+app.use(cors(corsOptions));
 server.applyMiddleware({app});
 
 app.listen({port}, async () => {
   console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
-  const foo = await query('SELECT NOW()');
-  console.log(foo);
 });
